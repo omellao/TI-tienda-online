b7c3cb5c05341bbdf2ec0b1755b278845869c1f4
CristobalSg <cristobal.sandoval2002@alu.uct.cl>
2022-10-21 17:07:07 -0300

    Input y boton para los mesajes, conectado con php, mongo

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,34 @@
 
 var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "leerColeccion.php");
+xhr.open("GET", "./php/leerColeccion.php");
 xhr.onload = function () {
     if (xhr.status == 200) {
         var json = JSON.parse(xhr.responseText);
         // console.log(json.mensajes[2].user);
-        console.log(json.mensajes[8].mensaje);
-        document.getElementById("respuesta").innerHTML =
-            json.mensajes[0].mensaje;
+        // console.log(json.mensajes.length);
+
+        // recorre todos los mensajes y los agrega al html
+        for (let i = 1; i < json.mensajes.length; i++) {
+            let node = document.createElement("div");
+
+            if (json.mensajes[i].user == "usuario1") {
+                node.className += "mensaje-user-1 mensaje";
+                let textnode = document.createTextNode(
+                    json.mensajes[i].mensaje
+                );
+                node.appendChild(textnode);
+            } else {
+                node.className += "mensaje-user-2 mensaje";
+                let textnode = document.createTextNode(
+                    json.mensajes[i].mensaje
+                );
+                node.appendChild(textnode);
+            }
+
+            document.getElementById("respuesta").appendChild(node);
+            // console.log(json.mensajes[i].user);
+        }
     } else {
         console.log("ERROR: " + xhr.status);
     }
--- php/agregarMensaje.php
+++ php/agregarMensaje.php
@@ -14,10 +14,13 @@
         }
     }
 
+    $mensaje = $_POST['mensaje'];
+    // echo $mensaje;
+
     // lo ideal es agregar la fecha como esto en los nuevos mensajes para luego ordenarlos
     //
-    // $fechaa = date('m-d-Y h:i:s a', time());
-    // $datoNuevo = ["user" => "usuario1","mensaje" => "texto...", "date" => $fechaa];
-    // $newData = new Crud();
-    // $noseKago = $newData->insertData($datoNuevo);
+    $fechaa = date('m-d-Y h:i:s a', time());
+    $datoNuevo = ["user" => "usuario2","mensaje" => "$mensaje", "date" => $fechaa];
+    $newData = new Crud();
+    $noseKago = $newData->insertData($datoNuevo);
 ?>
