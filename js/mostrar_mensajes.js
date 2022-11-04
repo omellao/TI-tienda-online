b2252eca042dbbcc8a59517a28af5e786647e66d
CristobalSg <cristobal.sandoval2002@alu.uct.cl>
2022-11-04 02:20:28 -0300

    Llamado de un solo chat - Junte la pag de chats con un chat

--- js/mostrar_mensajes.js
+++ js/mostrar_mensajes.js
@@ -2,28 +2,28 @@ async function getText(file) {
     const myObject = await fetch(file, {
         method: "POST",
     });
-    console.log(myObject);
-
+    // console.log(myObject);
     const obj = await myObject.json();
-    console.log(obj);
+    console.log(obj.mensajes);
 
-    // // recorre todos los mensajes y los agrega al html
-    // for (let i = 1; i < obj.mensajes.length; i++) {
-    //     let node = document.createElement("div");
+    document.getElementById("respuesta").innerHTML = "";
+    // recorre los mensajes y los agrega al html
+    for (let i = 1; i < obj.mensajes.length; i++) {
+        let node = document.createElement("div");
 
-    //     if (obj.mensajes[i].user == "usuario1") {
-    //         node.className += "mensaje-user-1 mensaje";
-    //         let textnode = document.createTextNode(obj.mensajes[i].mensaje);
-    //         node.appendChild(textnode);
-    //     } else {
-    //         node.className += "mensaje-user-2 mensaje";
-    //         let textnode = document.createTextNode(obj.mensajes[i].mensaje);
-    //         node.appendChild(textnode);
-    //     }
+        if (obj.mensajes[i].user == "usuario1") {
+            node.className += "mensaje-user-1 mensaje";
+            let textnode = document.createTextNode(obj.mensajes[i].mensaje);
+            node.appendChild(textnode);
+        } else {
+            node.className += "mensaje-user-2 mensaje";
+            let textnode = document.createTextNode(obj.mensajes[i].mensaje);
+            node.appendChild(textnode);
+        }
 
-    //     document.getElementById("respuesta").appendChild(node);
-    //     // console.log(obj.mensajes[i].user);
-    // }
+        document.getElementById("respuesta").appendChild(node);
+        // console.log(obj.mensajes[i].user);
+    }
 }
 
 // function activarChat() {
--- src/agregarMensaje.php
+++ src/agregarMensaje.php
@@ -14,15 +14,13 @@
         }
     }
 
-    // $user_1 = $_POST['usuario1'];
-    // $user_2 = $_POST['usuario2'];
     $id = $_POST['id_chat'];
 
     $mensaje = $_POST['mensaje'];
-    // echo $mensaje;
+
     // lo ideal es agregar la fecha como esto en los nuevos mensajes para luego ordenarlos
     $fechaa = date('m-d-Y h:i:s a', time());
-    $datoNuevo = ["emisor" => $user_1,"receptor" => $user_2,"mensaje" => "$mensaje", "date" => $fechaa];
+    $datoNuevo = ["id_chat" => $id,"mensaje" => "$mensaje", "date" => $fechaa];
     $newData = new Crud();
     $noseKago = $newData->insertData($datoNuevo);
 ?>
