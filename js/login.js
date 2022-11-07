ee58d6399452007cb05d1c73bddc5e32ea49fa18
pabloantivil <111023999+pabloantivil@users.noreply.github.com>
2022-11-07 18:56:21 -0300

    implementacion de FETCH para el login 1 hora
    implementacion de FETCH para el login 1 hora

--- js/login.js
+++ js/login.js
@@ -1,30 +1,21 @@
 const form = document.getElementById("login");
 
 const sendData = async dataUser => {
-  const response = await fetch('../../src/login.php', {
-      method: 'POST',
-      body: dataUser
-  });
+    const response = await fetch('../../src/login.php', {
+        method: 'POST',
+        body: dataUser
+    });
 
-  return await response.json();
+    return response.json();
 };
 
+
+
 form.addEventListener("submit", async event => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const userData = new FormData(form);
-  const status = await sendData(userData);
+    const userData = new FormData(form);
+    const status = await sendData(userData);
 
-  console.log(status);
+    console.log(status);
 });
-
-function validar(){
-  var username = document.getElementById("name").value;
-  var password = document.getElementById("pass").value;
-  if ( username == "Formget" && password == "formget#123"){
-  alert ("Logueado exitosamente");
-  window.location = "index.html"; // Redireccionado a otra pagina
-  return false;
-  }
-  else{}
-}
