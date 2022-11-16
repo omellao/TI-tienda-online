cc1d21119c830cd9e22ab2a60e7cdadae8629699
CristobalSg <cristobal.sandoval2002@alu.uct.cl>
2022-11-16 01:20:09 -0300

    css contactor

--- js/crear_contactos.js
+++ js/crear_contactos.js
@@ -23,15 +23,17 @@ async function getContact() {
         let node = document.createElement("li");
         // node.className += "un-contacto";
 
-        node.innerHTML =
-            "<div class='un-contacto'><img src=" +
-            "'../img/usuario.png'" +
-            "><p>" +
-            obj.users[i].user +
-            "</p><button onclick='cambiarNum(" +
-            contador +
-            ")'>Crear</button></div>";
-
+        node.innerHTML = `
+            <div class="un-contacto">
+                <div class="un-contacto1">
+                    <img src=../img/usuario.png>
+                    <p> ${obj.users[i].user}</p>
+                </div>    
+                <button onclick="cambiarNum(${contador})">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAWJJREFUSEvtVtFNw0AUsyeADYAJYAPoBMAGjAATUCYANugGlAloJyhMQNkAJjAyukRRkktzSUTy0SdFlZrX59jn55QYqTgSLvbA/6b8dKWWdAvgKEixJfk8hCxRxpKOAbwAOCsBvQO4Jrnt8wBNwCsA55HhK5KzwYElmeVmx+CTPqxrGUu6APC2A3hG0qpUSlKm1pqkZ1VqWsB+PEnfAA4irH9IHsYU6cw4AF8FV9fNt6uX2Y2wAdnK+eunsA3eAK9jVl+ZLxoDJJz1vODuNYB5+Wwluee+hcsfSLp3mJfEoMCSLPUlAAdJsRwcS5KvJamLfZb6FMBHSWon31/wVKRuSKyyktEESzaXJDv1E0DUsSV0O99B4s+8ugA3xWTMO5X4TAIOEpttl0qOz/yMw+vvsQsqgDuSNlTrKgK33cW64fl+tkUuAt8A8NWlFiQXKT+c7l+fFBYpvXvGKWr16h1N6l/7LJAfjlOuXQAAAABJRU5ErkJggg=="/>
+                </button>
+            </div>
+            `;
         contador += 1;
 
         // console.log(node);
@@ -99,14 +101,20 @@ document
     .getElementById("boton-contacto")
     .addEventListener("click", contactNone);
 
+// NEW
+document.getElementById("contac2").addEventListener("click", contactNone);
 // ESCONDER BARRA LATERAL
 var numNone = 0;
 function contactNone() {
     if (numNone == 0) {
         document.getElementById("contac").style.display = "block";
+        // NEW
+        document.getElementById("contac2").style.display = "block";
         numNone = 1;
     } else {
         document.getElementById("contac").style.display = "none";
+        // NEW
+        document.getElementById("contac2").style.display = "none";
         numNone = 0;
     }
 }
